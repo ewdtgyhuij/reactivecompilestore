80b53a1eba4c3792527e5144ee8610a429c2b30d
ewdtgyhuij <asjcndyhuisenga@hotmail.com>
2024-05-10 09:33:38 +0700

    update aptos task

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-const word = "hello";
-const reversedWord = word.split("").reverse().join("");
-console.log(reversedWord); // olleh
+const cycleSort = (arr) => {
+  for (let start = 0; start < arr.length - 1; start++) {
+    let item = arr[start];
+    let pos = start;
+    for (let i = start + 1; i < arr.length; i++) {
+      if (arr[i] < item) {
+        pos++;
+      }
+    }
+    if (pos === start) {
+      continue;
+    }
+    while (item === arr[pos]) {
+      pos++;
+    }
+    if (pos !== start) {
+      [item, arr[pos]] = [arr[pos], item];
+    }
+    while (pos !== start) {
+      pos = start;
+      for (let i = start + 1; i < arr.length; i++) {
+        if (arr[i] < item) {
+          pos++;
+        }
+      }
+      while (item === arr[pos]) {
+        pos++;
+      }
+      if (item !== arr[pos]) {
+        [item, arr[pos]] = [arr[pos], item];
+      }
+    }
+  }
+  return arr;
+};
