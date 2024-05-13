91e62e43444e2a78390ec7e1b094cb4843563eb1
ewdtgyhuij <asjcndyhuisenga@hotmail.com>
2024-05-13 07:43:13 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function isPalindrome(str) {
-  const reversed = str.split("").reverse().join("");
-  return str === reversed;
+function rob(nums) {
+  if (nums.length === 1) return nums[0];
+  const robRange = (start, end) => {
+    let prevMax = 0;
+    let currMax = 0;
+    for (let i = start; i <= end; i++) {
+      const temp = currMax;
+      currMax = Math.max(currMax, prevMax + nums[i]);
+      prevMax = temp;
+    }
+    return currMax;
+  };
+  return Math.max(robRange(0, nums.length - 2), robRange(1, nums.length - 1));
 }
-
-console.log(isPalindrome("racecar")); // true
